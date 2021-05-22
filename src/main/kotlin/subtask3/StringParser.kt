8e468a438448e0d477cc08dc84f0ef06fd1d4635
Dmitry Kucherenko <fatalzero@mail.ru>
2021-05-22 17:41:55 +0300

    Task 1 and Task2 and Task3 complete

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -1,9 +1,43 @@
 package subtask3
 
+import java.util.*
+import java.util.regex.Pattern
+import kotlin.collections.ArrayList
+
 class StringParser {
 
-    // TODO: Complete the following function
+
+
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+       val brackets = listOf('(','<','[',')','>',']')
+        var result = ArrayList<String>()
+       var s = StringBuilder(inputString)
+       for(i in s.length-1 downTo 0){
+           if(s[i]in brackets.subList(0,3)){
+              var last = 0
+               for(letter in i until s.length) {
+                   if (s[letter] == brackets[brackets.indexOf(inputString[i]) + 3]) {
+                       last = letter
+                       s[i] = ' '
+                       s[last] = ' '
+                       break
+                   }
+               }
+               result.add(0,inputString.substring(i+1,last))
+
+
+           }
+       }
+        return result.toTypedArray()
+
     }
 }
+
+
+
+
+
+
+
+
+
